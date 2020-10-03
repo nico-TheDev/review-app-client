0feaab88c56632ae86630bbf16653001f73b9177
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-03 14:22:21 +0800

    add global provider

--- src/util/Global.js
+++ src/util/Global.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "theme";
 
 export default function Global({ children }) {
     return (
         <>
             <CssBaseline />
-            {children}
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>{children}</ThemeProvider>
+            </BrowserRouter>
         </>
     );
 }
