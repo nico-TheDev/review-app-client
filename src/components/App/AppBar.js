09881afbb3049e19b2157e7975f605aa73835088
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-03 14:26:16 +0800

    add profile app nav abr

---
+++ src/components/App/AppBar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import clsx from "clsx";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+import Menu from "components/Menu";
+import { useStyles } from "./styles";
+
+export default function AppBarComponent({ open,handleDrawerOpen }) {
+    const classes = useStyles();
+
+    return (
+        <AppBar
+            position="fixed"
+            className={clsx(classes.appBar, {
+                [classes.appBarShift]: open,
+            })}
+        >
+            <Toolbar>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    edge="start"
+                    className={clsx(classes.menuButton, open && classes.hide)}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" noWrap>
+                    Dashboard
+                </Typography>
+
+                <Menu />
+            </Toolbar>
+        </AppBar>
+    );
+}
