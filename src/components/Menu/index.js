4e26b359378e90cf829b38d186ea680ebfad4d1f
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-03 14:23:07 +0800

    add Nav Menu Component

---
+++ src/components/Menu/index.js
@@ -0,0 +1,49 @@
+import React,{ useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { Avatar } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    avatarStyle: {
+        marginLeft: "auto",
+    },
+});
+
+export default function SimpleMenu() {
+    const classes = useStyles();
+    //state for anchor element
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div className={classes.avatarStyle}>
+            <Button
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+            >
+                <Avatar />
+            </Button>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+        </div>
+    );
+}
