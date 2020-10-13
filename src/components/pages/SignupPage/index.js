89b6fef7893e62275d328d58e25ce05ee903d9a5
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-13 18:05:54 +0800

    add signup validation

--- src/components/pages/SignupPage/index.js
+++ src/components/pages/SignupPage/index.js
@@ -1,11 +1,8 @@
-import React from "react";
-import { Link as RouterLink } from "react-router-dom";
+import React, { useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
 import PersonIcon from "@material-ui/icons/AccountCircle";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
@@ -39,6 +36,69 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignIn() {
     const classes = useStyles();
+    const [signupData, setSignupData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        passwordTwo: "",
+    });
+    const [errorHandler, setErrorHandler] = useState({
+        firstName: { hasError: false, msg: "" },
+        lastName: { hasError: false, msg: "" },
+        email: { hasError: false, msg: "" },
+        password: { hasError: false, msg: "" },
+        passwordTwo: { hasError: false, msg: "" },
+    });
+
+    const handleChange = (e) => {
+        const current = e.target.name;
+        const newData = {
+            ...signupData,
+        };
+        newData[current] = e.target.value;
+
+        setSignupData(newData);
+        
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+
+        if (
+            Object.values(signupData).every((item) => item !== "") &&
+            signupData.password === signupData.passwordTwo
+        ) {
+            alert("sent");
+        } else {
+            const keys = Object.keys(signupData);
+            let newErrorState = { ...errorHandler };
+            Object.values(signupData).forEach((val, i) => {
+                if (val === "") {
+                    newErrorState[keys[i]] = { hasError: true, msg: "Cannot be empty" };
+                } else if (!regex.test(signupData.email)) {
+                    newErrorState.email = {
+                        hasError: true,
+                        msg: "Incorrect Email",
+                    };
+                } else if (signupData.password.length < 6) {
+                    newErrorState.password = {
+                        hasError: true,
+                        msg: "Password minimum of 6 characters",
+                    };
+                } else if (signupData.password !== signupData.passwordTwo) {
+                    newErrorState.passwordTwo = {
+                        hasError: true,
+                        msg: "Password should be the same",
+                    };
+                } else {
+                    newErrorState[keys[i]] = { hasError: false, msg: "" };
+                }
+            });
+            setErrorHandler(newErrorState);
+        }
+    };
 
     return (
         <Container component="main" maxWidth="xs">
@@ -50,39 +110,82 @@ export default function SignIn() {
                 <Typography component="h1" variant="h5">
                     Create Account
                 </Typography>
-                <form className={classes.form} noValidate>
+                <form
+                    className={classes.form}
+                    noValidate
+                    autoComplete="off"
+                    onSubmit={handleSubmit}
+                >
                     <TextField
                         variant="outlined"
                         margin="normal"
-                        required
                         fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
+                        id="firstName"
+                        label="First Name"
+                        name="firstName"
+                        value={signupData.firstName}
                         autoFocus
+                        onChange={handleChange}
+                        error={errorHandler.firstName.hasError}
+                        helperText={errorHandler.firstName.msg}
+                        required
                     />
                     <TextField
                         variant="outlined"
                         margin="normal"
+                        fullWidth
+                        id="lastName"
+                        label="Last Name"
+                        name="lastName"
+                        value={signupData.lastName}
+                        autoFocus
+                        error={errorHandler.lastName.hasError}
+                        helperText={errorHandler.lastName.msg}
+                        onChange={handleChange}
                         required
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        value={signupData.email}
+                        autoFocus
+                        type="email"
+                        error={errorHandler.email.hasError}
+                        helperText={errorHandler.email.msg}
+                        onChange={handleChange}
+                        required
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
                         fullWidth
                         name="password"
                         label="Password"
                         type="password"
                         id="password"
-                        autoComplete="current-password"
+                        error={errorHandler.password.hasError}
+                        helperText={errorHandler.password.msg}
+                        value={signupData.password}
+                        onChange={handleChange}
+                        required
                     />
                     <TextField
                         variant="outlined"
                         margin="normal"
-                        required
                         fullWidth
                         name="passwordTwo"
                         label="Re-enter Password"
                         type="password"
                         id="passwordTwo"
-                        autoComplete="current-password"
+                        error={errorHandler.passwordTwo.hasError}
+                        helperText={errorHandler.passwordTwo.msg}
+                        value={signupData.passwordTwo}
+                        onChange={handleChange}
+                        required
                     />
                     <Button
                         type="submit"
