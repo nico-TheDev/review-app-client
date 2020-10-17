5b799b0350356f48d3d18dbc14c86a2ca7bd8e89
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-17 22:05:11 +0800

    make authContext use localStorage

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,30 +1,46 @@
 import React, { useReducer, useEffect, createContext, useContext } from "react";
-import Cookie from "js-cookie";
-
+import { useHistory } from "react-router-dom";
+import api from "api/reviewapp.instance";
 import AuthReducer from "reducers/AuthReducer";
-
+import ActionTypes from "actions/ActionTypes";
 const AuthContext = createContext();
 
 const initialState = () => {
-    if (Cookie.get("jwt"))
+    if (localStorage.getItem("token"))
         return {
-            token: Cookie.get("jwt"),
+            token: localStorage.getItem("token"),
             user: null,
+            userID: localStorage.getItem("userID"),
         };
 
     return {
         token: null,
         user: null,
+        userID: null,
     };
 };
 
 export const AuthProvider = ({ children }) => {
     const [authState, authDispatch] = useReducer(AuthReducer, {}, initialState);
-
+    const history = useHistory();
+    // GET USER DETAILS
     useEffect(() => {
         if (authState.token && !authState.user) {
+            api.get(`/user/${authState.userID}`, {
+                params: { token: authState.token },
+            })
+                .then((res) => {
+                    authDispatch({
+                        type: ActionTypes.SET_USER,
+                        user: res.data.user,
+                    });
+                    history.push("/dashboard");
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
         }
-    }, [authState, authDispatch]);
+    }, [authState]);
 
     return (
         <AuthContext.Provider value={{ authState, authDispatch }}>
