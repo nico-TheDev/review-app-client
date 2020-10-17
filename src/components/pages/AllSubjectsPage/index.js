e17f6b061c63b3c817ffcdddf3b6cfeb3204702f
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-17 22:03:48 +0800

    fix data checking and fix all subjects fetching

--- src/components/pages/AllSubjectsPage/index.js
+++ src/components/pages/AllSubjectsPage/index.js
@@ -1,11 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-    Typography,
-    Grid,
-    Divider,
-    IconButton,
-    Fab,
-} from "@material-ui/core";
+import { Typography, Grid, Divider, IconButton, Fab } from "@material-ui/core";
 import FilterListIcon from "@material-ui/icons/FilterList";
 import CreateIcon from "@material-ui/icons/Create";
 
@@ -13,33 +7,39 @@ import SubjectCard from "./SubjectCard";
 import Modal from "./SubjectModal";
 
 import api from "api/reviewapp.instance";
-import useStyles from 'components/shared/fabUseStyle'
-import { useModal } from 'contexts/ModalContext'
-import ActionTypes from 'actions/ActionTypes'
-
+import useStyles from "components/shared/fabUseStyle";
+import { useModal } from "contexts/ModalContext";
+import { useAuth } from "contexts/AuthContext";
+import ActionTypes from "actions/ActionTypes";
 
 export default function Subjects() {
     const classes = useStyles();
-    const { state,dispatch } = useModal();
+    const { state, dispatch } = useModal();
+    const { authState, authDispatch } = useAuth();
     const [subjects, setSubjects] = useState([]);
+    const { token, userID } = authState;
 
     useEffect(() => {
-        api.get("/subject/all")
+        api.get(`/subject/all/${userID}`, {
+            params: {
+                token,
+            },
+        })
             .then((res) => {
+                console.log(res);
                 setSubjects(res.data);
             })
             .catch((err) => {
                 console.log(err);
             });
-    }, [state.isSubjectModalOpen]);
+    }, [state.isSubjectModalOpen, token, userID]);
 
     const handleOpen = () => {
-        dispatch({type:ActionTypes.OPEN_SUBJECT_MODAL})
+        dispatch({ type: ActionTypes.OPEN_SUBJECT_MODAL });
     };
 
     return (
         <Grid container spacing={2} className={classes.containerStyle}>
-
             <Grid item xs={12} container justify="space-between">
                 <Typography variant="h4" color="primary">
                     Subjects
@@ -53,7 +53,7 @@ export default function Subjects() {
             </Grid>
 
             <Grid item container xs={12} spacing={2} wrap="wrap">
-                {subjects.length ? (
+                {Array.isArray(subjects) && subjects.length ? (
                     subjects.map((item, index) => (
                         <SubjectCard key={index} details={item} />
                     ))
@@ -70,9 +70,7 @@ export default function Subjects() {
             >
                 <CreateIcon />
             </Fab>
-            {state.isSubjectModalOpen && (
-                <Modal/>
-            )}
+            {state.isSubjectModalOpen && <Modal />}
         </Grid>
     );
 }
