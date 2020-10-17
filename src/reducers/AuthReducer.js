bb81964a9881dbd922989293b5820647d912d506
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-17 22:05:31 +0800

    add setUser and setToken functions

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,9 +1,24 @@
 import ActionTypes from "actions/ActionTypes";
 
+function saveToLocalStorage(token, id) {
+    localStorage.setItem("token", token);
+    localStorage.setItem("userID", id);
+}
+
 export default function AuthReducer(state, action) {
     switch (action.type) {
-        case ActionTypes.SIGNUP_USER:
-            return state;
+        case ActionTypes.SET_TOKEN:
+            saveToLocalStorage(action.token, action.userID);
+            return {
+                ...state,
+                token: action.token,
+                userID: action.userID,
+            };
+        case ActionTypes.SET_USER:
+            return {
+                ...state,
+                user: action.user,
+            };
         case ActionTypes.LOGIN_USER:
             return state;
         case ActionTypes.LOGOUT_USER:
