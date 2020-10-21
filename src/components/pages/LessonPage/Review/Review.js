67e85caa27e7c4ba68439341f27cebcc23628491
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-21 17:27:10 +0800

    add minor texts

---
+++ src/components/pages/LessonPage/Review/Review.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Grid, Typography } from "@material-ui/core";
+
+export default function Review() {
+    return (
+        <Grid xs={10} item>
+            REVIEW SHITS HERE
+        </Grid>
+    )
+}
--- src/components/pages/LessonPage/ReviewSubheading.js
+++ src/components/pages/LessonPage/ReviewSubheading.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Grid, Typography } from "@material-ui/core";
-import formatDate from "util/formatDate";
 
 export default function NSubhead({ details }) {
     return (
         <>
-            <Grid item xs={6}>
+            <Grid item xs={3}>
                 <Typography variant="body1" component="p">
                 Highest Score : None
                 </Typography>
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={3}>
+                <Typography variant="body1" component="p">
+                Number of Tries: 0
+                </Typography>
+            </Grid>
+            <Grid item xs={3}>
                 <Typography variant="body1" component="p">
                 Number of Questions: 0
                 </Typography>
