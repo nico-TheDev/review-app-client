6a332e5e272865665f4cf7303ec2b41775b7f64b
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-22 14:50:40 +0800

    add review using reviewcard

--- src/components/pages/LessonPage/Review/Review.js
+++ src/components/pages/LessonPage/Review/Review.js
@@ -1,10 +1,32 @@
-import React from 'react'
-import { Grid, Typography } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Grid } from "@material-ui/core";
 
-export default function Review() {
+import api from "api/reviewapp.instance";
+import ReviewCard from "./ReviewCard";
+
+export default function Review({ update,handleOpenEdit,setTargetQuestion }) {
+    const { lessonID } = useParams();
+    const [questionList, setQuestionList] = useState([]);
+
+    useEffect(() => {
+        api.get(`/question/${lessonID}/all`)
+            .then((res) => setQuestionList(res.data))
+            .catch((err) => console.log(err));
+    }, [update, lessonID]);
+
     return (
-        <Grid xs={10} item>
-            REVIEW SHITS HERE
+        <Grid xs={10} item container spacing={4}>
+            {questionList.length
+                ? questionList.map((question, count) => (
+                      <ReviewCard
+                          key={question._id}
+                          question={{ ...question, count }}
+                          handleOpenEdit={handleOpenEdit}
+                          setTargetQuestion={setTargetQuestion}
+                      />
+                  ))
+                : "No Questions Yet"}
         </Grid>
-    )
+    );
 }
