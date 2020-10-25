9d96f6792fa9a5306c12313c3d12f31851d41cb3
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-25 16:06:35 +0800

    save function to ntoes

--- src/components/pages/LessonPage/TextEditor.js
+++ src/components/pages/LessonPage/TextEditor.js
@@ -1,8 +1,7 @@
-import React, { useMemo, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Editable, withReact, useSlate, Slate } from "slate-react";
-import { createEditor, Node } from "slate";
-import { Grid, Paper, Button } from "@material-ui/core";
+
+import { Grid, Paper, Button, TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 import api from "api/reviewapp.instance";
@@ -13,85 +12,57 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-// Define a serializing function that takes a value and returns a string.
-const serialize = (value) => {
-    return value.map((n) => Node.string(n)).join("\n");
-};
-
-// Define a deserializing function that takes a string and returns a value.
-const deserialize = (string) => {
-    return string.split("\n").map((line) => {
-        return {
-            children: [{ text: line }],
-        };
-    });
-};
-
 export default function TextEditor() {
     const { lessonID } = useParams();
     const classes = useStyles();
-    const editor = useMemo(() => withReact(createEditor()), []);
-    const [value, setValue] = useState([
-        {
-            type: "paragraph",
-            children: [{ text: "A line of text in a paragraph." }],
-        },
-    ]);
+    const [value, setValue] = useState("");
 
-    // CREATE A NOTE
     useEffect(() => {
-        const getNote = async () => {
-            try {
-                const res = await api.get(`/note/${lessonID}`);
-                const value = deserialize(res.data.notes);
-                setValue(value);
-            } catch (err) {
-                console.log(err);
-                console.log("ERROR");
-            }
-        };
-        getNote();
+        api.get(`/note/${lessonID}`)
+            .then((res) => {
+                console.log(res)
+                setValue(res.data.notes);
+            })
+            .catch((err) => console.log(err));
     }, [lessonID]);
 
     const handleSave = async () => {
         try {
-            const update = await api.put(`/note/${lessonID}`, {
-                data: {
-                    notes: serialize(value),
-                },
-            });
-            console.log("Updated", update);
+            const getRes = await api.get(`/note/${lessonID}`);
+            if (getRes.data.notes) {
+                const update = await api.put(`note/${lessonID}`, {
+                    data: {
+                        lessonID,
+                        notes: value,
+                    },
+                });
+            } else {
+                const create = await api.post(`note/$create`, {
+                    data: {
+                        lessonID,
+                        notes: value,
+                    },
+                });
+            }
         } catch (err) {
-            const newNote = await api.post(`/note/create`, {
-                data: {
-                    lessonID,
-                    notes: serialize(value),
-                },
-            });
-            console.log("created", newNote);
+            console.log(err);
         }
     };
-
     const handleClear = () => {
-        setValue([
-            {
-                type: "paragraph",
-                children: [{ text: "Start writing" }],
-            },
-        ]);
+        setValue("");
     };
 
     return (
         <>
             <Grid item xs={10}>
                 <Paper elevation={4} className={classes.paper}>
-                    <Slate
-                        editor={editor}
+                    <TextField
+                        fullWidth
+                        multiline
+                        placeholder="Start taking notes"
+                        onChange={(e) => setValue(e.target.value)}
                         value={value}
-                        onChange={(newValue) => setValue(newValue)}
-                    >
-                        <Editable />
-                    </Slate>
+                    />
                 </Paper>
             </Grid>
             <Grid item container xs={10} spacing={3}>
