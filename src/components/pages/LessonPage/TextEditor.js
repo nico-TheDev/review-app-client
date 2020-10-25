ecd4211563085ac68f7e63883142a878e8ac6d6e
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-25 15:25:40 +0800

    try note add

--- src/components/pages/LessonPage/TextEditor.js
+++ src/components/pages/LessonPage/TextEditor.js
@@ -1,7 +1,7 @@
 import React, { useMemo, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Editable, withReact, useSlate, Slate } from "slate-react";
-import { createEditor } from "slate";
+import { createEditor, Node } from "slate";
 import { Grid, Paper, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -13,19 +13,73 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+// Define a serializing function that takes a value and returns a string.
+const serialize = (value) => {
+    return value.map((n) => Node.string(n)).join("\n");
+};
+
+// Define a deserializing function that takes a string and returns a value.
+const deserialize = (string) => {
+    return string.split("\n").map((line) => {
+        return {
+            children: [{ text: line }],
+        };
+    });
+};
+
 export default function TextEditor() {
+    const { lessonID } = useParams();
     const classes = useStyles();
     const editor = useMemo(() => withReact(createEditor()), []);
     const [value, setValue] = useState([
         {
             type: "paragraph",
-            children: [{ text: "Start Taking notes" }],
+            children: [{ text: "A line of text in a paragraph." }],
         },
     ]);
 
     // CREATE A NOTE
+    useEffect(() => {
+        const getNote = async () => {
+            try {
+                const res = await api.get(`/note/${lessonID}`);
+                const value = deserialize(res.data.notes);
+                setValue(value);
+            } catch (err) {
+                console.log(err);
+                console.log("ERROR");
+            }
+        };
+        getNote();
+    }, [lessonID]);
 
-    useEffect(() => {}, []);
+    const handleSave = async () => {
+        try {
+            const update = await api.put(`/note/${lessonID}`, {
+                data: {
+                    notes: serialize(value),
+                },
+            });
+            console.log("Updated", update);
+        } catch (err) {
+            const newNote = await api.post(`/note/create`, {
+                data: {
+                    lessonID,
+                    notes: serialize(value),
+                },
+            });
+            console.log("created", newNote);
+        }
+    };
+
+    const handleClear = () => {
+        setValue([
+            {
+                type: "paragraph",
+                children: [{ text: "Start writing" }],
+            },
+        ]);
+    };
 
     return (
         <>
@@ -42,12 +96,22 @@ export default function TextEditor() {
             </Grid>
             <Grid item container xs={10} spacing={3}>
                 <Grid item xs={6}>
-                    <Button variant="outlined" color="primary" fullWidth>
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        fullWidth
+                        onClick={handleSave}
+                    >
                         SAVE
                     </Button>
                 </Grid>
                 <Grid item xs={6}>
-                    <Button variant="outlined" color="secondary" fullWidth>
+                    <Button
+                        variant="outlined"
+                        color="secondary"
+                        fullWidth
+                        onClick={handleClear}
+                    >
                         CLEAR
                     </Button>
                 </Grid>
